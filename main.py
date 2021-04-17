a4270a86d35cd9efb67c96365194d33ebe9d2cb1
Zeeshan Sardar <mzeeshan@xavor.com>
2021-04-17 23:56:31 +0500

    added comments

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
+# import packages
 import serial 
 from serial.tools import list_ports
 
+# list the plugged devices
 portList = list(list_ports.comports())
 
-f = open("arduino_data.txt", "r")
+# open file to read header (column names)
+print('Reading column names..')
+f = open("data_log.txt", "r")
 file_header = f.readline().split('\t')[:-1]
 f.close()
 
-with open("arduino_data.txt", "a") as f:
-
+# open file to append data
+print('Opening file to append data..')
+with open("data_log.txt", "a") as f:
+# main loop
+print('Entering into main loop..')
     while True:
         portList = list(list_ports.comports())
         row_list = [',', ',', ',', ',', '\n']
-#         print(len(portList))
+        # loop over the available ports
         for port in portList:
             try:
+                # open serial port
                 ser_handle = serial.Serial(port.device, 19200, timeout=2)
-                data = ser_handle.readline().decode('utf-8').rstrip()
-
-                data = data+','
-
-#                 print(type(data))
+                # read serial data
+                data = ser_handle.readline().decode('utf-8').rstrip()+','
+                # close the port
                 ser_handle.close()
+                # get the corresponding device ID
                 device_id = port.hwid.split()[2][4:]
+                # match the column name
                 col_idx = file_header.index(device_id)
+                # put data into list
                 row_list[col_idx] = data
 
             except:
                 continue 
-
+        # count empty data fields out of four ports
         empty_data_fields = row_list.count(',')
+        # check if the there is data available from at least one port
         if empty_data_fields < 4: 
             print(f'Reading from {4-empty_data_fields} devices.')
-            # print(row_list)
+            # write into file
             f.writelines(row_list)
             f.flush()
-
-
     f.close()
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from serial.tools import list_ports
 
+# get available ports
 device_ids = []
 portList = list(list_ports.comports())
+
+# get device IDs of the available ports
 for port in portList:
     device_ids.append(port.hwid.split()[2][4:]+"\t")
 device_ids.append('\n')
 
+# writing the IDs as column names of the log data file
 with open("arduino_data.txt", "w") as f:
     f.writelines(device_ids)
     f.close()
