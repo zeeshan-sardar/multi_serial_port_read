90be2d216565e9a9891d93ff3bc237eea906b52a
Zeeshan Sardar <mzeeshan@xavor.com>
2021-04-18 00:02:14 +0500

    added arduino sketches

---
+++ arduino_files/sketch1.ino
@@ -0,0 +1,10 @@
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+
+  Serial.println(2);
+  delay(2000);
+}
---
+++ arduino_files/sketch3.ino
@@ -0,0 +1,10 @@
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+
+  Serial.println(4);
+  delay(2000);
+}
